5938fbbb4e6a4f9ba372ffc1f79e5b94c036020f
Ginatam32 <fluffey6@gmail.com>
2025-02-02 16:49:24 -0600

    changed port number in index

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import app from './app.js';
 
-import db from './database.js';
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server is running on port 3000...');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT}...`);
 });
