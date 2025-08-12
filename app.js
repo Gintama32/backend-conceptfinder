d8dce63b89902cfa37635c4aed33f0b2bec41c45
Ginatam32 <fluffey6@gmail.com>
2025-08-11 19:00:51 -0700

    fix cors config

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
+
+// CORS configuration
 app.use(cors({
-  origin: 'https://zippy-kleicha-99f2cc.netlify.app'
+  origin: 'https://zippy-kleicha-99f2cc.netlify.app',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 app.use(express.json());
 import routers from "./routes/routes.js";
 app.use('/api/v1/', routers);
-
-
 
 export default app;
